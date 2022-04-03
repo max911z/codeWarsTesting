4a7a35343cebf7f7b71419bf0e22617a8815ff32
Maksim Neverovskiy <80741988+max911z@users.noreply.github.com>
2022-04-03 22:00:12 +0700

    Add unit-tests

---
+++ codeWarsTests.swift
@@ -0,0 +1,39 @@
+//
+//  codeWarsTests.swift
+//  codeWarsTests
+//
+//  Created by Максим Неверовский on 29.03.2022.
+//
+import Foundation
+import XCTest
+
+class FoldArray{
+    func foldArray(array: Array<Int>, times: Int = 1) -> [Int]{
+        var nums = array
+        for _ in 0..<1{
+            for row in 0..<(nums.count / 2) {
+                nums[row] += nums.popLast()!
+            }
+        }
+        return nums
+    }
+}
+
+class codeWarsTests: XCTestCase {
+    var foldArray = FoldArray()
+    
+    func testWhenInputIsEmptyArray() {
+        let array = [] as [Int]
+        let result = foldArray.foldArray(array: array)
+        XCTAssertEqual(result, [])
+    }
+    
+    func testWhenInputIsNegativeAndPositiveNumbers() {
+        let array = [-9, 10, -100, -234, 34, 5] as [Int]
+        let result = foldArray.foldArray(array: array)
+        XCTAssertEqual( result, [-4, 44, -334])
+    }
+    
+    
+    
+}
